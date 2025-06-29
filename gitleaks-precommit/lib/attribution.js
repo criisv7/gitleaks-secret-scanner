2b35c88abc3e02f8f818bef950528f15a37bd91f
Vijay <vijay.k@pei.group>
2025-06-29 19:32:52 +0530

    Version Bump

--- gitleaks-precommit/lib/attribution.js
+++ gitleaks-precommit/lib/attribution.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 
 const fallbackPackageInfo = {
   name: "gitleaks-secret-scanner",
-  version: "1.1.2",
+  version: "1.1.3",
   repository: { url: "https://github.com/criisv7/gitleaks-secret-scanner" },
 };
 
