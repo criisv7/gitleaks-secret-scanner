ce3114880ad8930ef2327693afe4b6ad953f816e
Vijay <vijay.k@pei.group>
2025-07-01 00:01:10 +0530

    Updated Readme.md file

--- gitleaks-precommit/lib/attribution.js
+++ gitleaks-precommit/lib/attribution.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 
 const fallbackPackageInfo = {
   name: "gitleaks-secret-scanner",
-  version: "1.2.1",
+  version: "1.2.2",
   repository: { url: "https://github.com/criisv7/gitleaks-secret-scanner" },
 };
 
